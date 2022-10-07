0d236a396f7488ab15edafbd25b05aafc7dfeb04
Alina Lebedeva <111756730+LebedevaAlina-zp@users.noreply.github.com>
2022-10-08 02:20:23 +0400

    Update Some_Exercices.py

--- Module 13/Some_Exercices.py
+++ Module 13/Some_Exercices.py
@@ -14,7 +14,7 @@ Multi_table = [[str(i)+'*'+str(j)+'='+str(i*j) for j in range(1, i+1)] for i in 
 for row in Multi_table:
     print(row)
     
-#Задание 13.8.17
+#13.8.17
 #Используя функцию zip() внутри генераторов списков, вычислите поэлементные произведения списков.
 # Using zip() function inside list comprehension calculate the products of corresponding element multiplication of the lists:  
 # L = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  и M = [0, 9, 8, 7, 6, 5, 4, 3, 2, 1]
@@ -24,3 +24,25 @@ M = [i for i in range(9, 0, -1)]
 
 N = [a*b for a,b in zip(L,M)]
 print(N)
+
+# 13.8.18
+# Реализуйте программу, которая сжимает последовательность символов. На вход подается последовательность вида:
+# aaabbccccdaa
+# Необходимо вывести строку, состоящую из символов и количества повторений этого символа. Вывод должен выглядеть как:
+# a3b2c4d1a2
+# Write a program that converts a test string like 'aaabbccccdaa' into a string like 'a3b2c4d1a2' consisting of symbols and the numbers of their repetitions in a row.
+
+s = input('Enter some text: ')
+
+s_new = s[0]
+letter_count = 1
+
+for i in range(1, len(s)):
+    if s[i] == s[i-1]:
+        letter_count += 1
+    else:
+        s_new += str(letter_count) + s[i]
+        letter_count = 1
+s_new += str(letter_count)
+
+print(s_new)
