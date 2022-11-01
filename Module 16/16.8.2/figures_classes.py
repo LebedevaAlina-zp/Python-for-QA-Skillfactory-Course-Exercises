5cdda0452c4a7086ad0435550bdc7af374fd1b21
Alina Lebedeva <111756730+LebedevaAlina-zp@users.noreply.github.com>
2022-11-02 02:30:10 +0400

    Update figures_classes.py
    According to task 16.10.5 an Exception for non-positive values was added.

--- Module 16/16.8.2/figures_classes.py
+++ Module 16/16.8.2/figures_classes.py
@@ -1,9 +1,16 @@
 # Here are the created classes: Rectangle, Square and Circle, for each one can get its area.
+# Task 16.10.5. Add your own NonPositiveDigitException to the Square class constructor, inherited from ValueError, 
+# which is triggered when the side of the square is less than or equal to 0.
 
 import math
 
+class NonPositiveDigitException(ValueError):
+    pass
+
 class Rectangle:
     def __init__(self, width, height):
+        if width <= 0 or height <= 0:
+            raise NonPositiveDigitException("The rectangle's width and height can't take negative values or 0")
         self.width = width
         self.height = height
 
@@ -19,6 +26,8 @@ class Rectangle:
 
 class Square:
     def __init__(self, side):
+        if side <= 0:
+            raise NonPositiveDigitException("The square's side can't take negative values or 0")
         self.side = side
 
     def getArea_square(self):
@@ -27,6 +36,8 @@ class Square:
 
 class Circle:
     def __init__(self, radius):
+        if radius <= 0:
+            raise NonPositiveDigitException("The circle's radius can't take negative values or 0")
         self.radius = radius
 
     def getArea_circle(self):
