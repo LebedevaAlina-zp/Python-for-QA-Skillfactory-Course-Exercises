34912339f30a1b6ad6759056d42fe75ed3eb9ece
LebedevaAlina-zp <lebedeva.alina.zp@gmail.ru>
2022-11-19 03:40:44 +0400

    Добавлена обработка команды /values и основа калькулятора валют Реализован основной функционал бота, далее надо обработать ошибки ввода.

--- Module 18 Final project Telebot/config.py
+++ Module 18 Final project Telebot/config.py
@@ -1,1 +1,7 @@
 TOKEN = '5707068656:AAEeUpr3XwQQet1VYVzCRIyEiKFQmKveN9o'
+
+currencies = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
--- Module 18 Final project Telebot/currencies_telebot_app.py
+++ Module 18 Final project Telebot/currencies_telebot_app.py
@@ -1,15 +1,43 @@
+import requests
 import telebot
-from config import TOKEN
+import json
+from extensions import APIException
+from config import TOKEN, currencies
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def handle_start(message: telebot.types.Message):
     text = '''Это бот-калькулятор конвертации валют. Введите команду в формате:
-    <валюта, из которой переводим> <валюта, в которую переводим> <сумма в первой валюте>
+    <валюта, в которую переводим> <валюта, из которой переводим> <количество первой валюты>
     Например: доллар рубль 100
     Посмотреть список досутпных валют можно по команде /values'''
     bot.send_message(message.chat.id, text)
 
-    
+
+@bot.message_handler(commands=['values'])
+def handle_values(message: telebot.types.Message):
+    text = ''
+    for key in currencies.keys():
+        text = '\n'.join((text, key))
+    text = f"Список доступных валют:\n {text}"
+    bot.send_message(message.chat.id, text)
+
+
+@bot.message_handler(content_types=['text'])
+def curr_converter(message: telebot.types.Message):
+    params = message.text.split()
+
+    if len(params) != 3:
+        raise APIException("Команда должна содержать ровно 3 параметра.")
+
+    quote, base, amount = params
+
+    r = requests.get(f'https://api.exchangerate.host/convert?from={currencies[quote]}&to={currencies[base]}')
+    total_base = r.json()['result'] * float(amount)
+    text = f"{amount} {quote} - это {total_base} {base} по курсу https://exchangerate.host"
+
+    bot.reply_to(message, text)
+
+
 bot.polling(none_stop=True)
