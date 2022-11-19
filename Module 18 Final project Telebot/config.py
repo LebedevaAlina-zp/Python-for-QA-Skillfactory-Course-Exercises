d05bb5f2b9a70c0e35dcc26c490039d17fc66771
Alina Lebedeva <111756730+LebedevaAlina-zp@users.noreply.github.com>
2022-11-20 03:50:27 +0400

    Добавлены еще валюты.
    В связи с добавлением новых валют решено хранить альтернативные имена каждой из валют в словаре currencies.

--- Module 18 Final project Telebot/config.py
+++ Module 18 Final project Telebot/config.py
@@ -1,12 +1,11 @@
 TOKEN = '5707068656:AAEeUpr3XwQQet1VYVzCRIyEiKFQmKveN9o'
 
 currencies = {
-    'доллар': ['USD', 'долларов'],
-    'евро': ['EUR', 'евро'],
-    'рубль': ['RUB', 'рублей'],
+    'доллар': ['USD', 'долларов', ('usd', 'долар', 'доллары', 'dollar', 'dollars', '$')],
+    'евро': ['EUR', 'евро', ('eur', 'euros', 'eu')],
+    'фунт': ['GBP', 'фунтов', ('фунты', 'gbp', 'pound', 'pounds')],
+    'иена': ['JPY', 'иен', ('jpy', 'иены', 'yen', 'йена', 'йены')],
+    'юань': ['CNY', 'юаней', ('cny', 'юани')],
+    'рубль': ['RUB', 'рублей', ('рубли', 'rub', 'ruble', 'rubl', 'rubles')],
+    'франк': ['CHF', 'швейцарских франков', ('chf', 'франки', 'frank')],
  }
-
-dollar_names = ['usd', 'долар', 'доллары', 'dollar', 'dollars', '$']
-euro_names = ['eur', 'euros', 'eu']
-ruble_names = ['рубли', 'rub', 'ruble', 'rubl', 'rubles']
-
